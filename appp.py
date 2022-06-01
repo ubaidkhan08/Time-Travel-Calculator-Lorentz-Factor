ef019529f036846fbbf1396ed51ff37e2ec74f5b
ubaidkhan08 <88443025+ubaidkhan08@users.noreply.github.com>
2022-06-01 06:48:00 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -11,26 +11,30 @@ def main():
 
     #Choice = st.radio("What do you wish to do?",('Necessary Velocity for Time Dilation', 'Calculate Time Dilation at any Velocity'))
     
-    if st.button('Necessary Velocity for Time Dilation'):
+    #if st.button('Necessary Velocity for Time Dilation'):
     #if Choice == 'Necessary Velocity for Time Dilation':
-        a = st.slider('How many years of dilation?', 0, 100) #Time_Travel
-        b = st.slider('Expendable time in Years before the dilation (lesser the time, more the risk)', 0, 100) #Preferred_Duration
-        if a<b:
-            st.text("Dilation can not be in negative, yet, genius!")
+    st.subheader('Necessary Velocity for Time Dilation Calculation')
+    a = st.slider('How many years of dilation?', 0, 100) #Time_Travel
+    b = st.slider('Expendable time in Years before the dilation (lesser the time, more the risk)', 0, 100) #Preferred_Duration
+    if a<b:
+        st.text("Dilation can not be in negative, yet, genius!")
        
-        if st.button('Calculate the Velocity'):
-            output = round(((np.sqrt(1 - (1/((a/b)*(a/b)))))*100),3)
-            st.success('Your velocity should be: {}% of the speed of light.'.format(output))
+    if st.button('Calculate the Velocity'):
+        output = round(((np.sqrt(1 - (1/((a/b)*(a/b)))))*100),3)
+        st.success('Your velocity should be: {}% of the speed of light.'.format(output))
     
-    if st.button('Calculate Time Dilation at any Velocity'):
+    st.text("\n")
+    st.text("\n")
+    st.subheader('Calculate Time Dilation at any Velocity')
+    #if st.button('Calculate Time Dilation at any Velocity'):
     #if Choice == 'Calculate Time Dilation at any Velocity':
-        c = st.slider('Enter Current Velocity (in m/sec)', 0, 299792458) #Current_Velocity
-        if st.button('Calculate Time Dilation'):
-            gamma = (1 / (np.sqrt(1-c*c)))
-            gammar = round((1 / (np.sqrt(1-c*c))),3)
-            monthz = round((12-(12/gamma)),3)
-            st.success('The Time Dilation would be: {} times'.format(gammar))
-            st.text("Or you will experience: {} lesser month(s).".format(monthz))
+    c = st.slider('Enter Current Velocity (in m/sec)', 0, 299792458) #Current_Velocity
+    if st.button('Calculate Time Dilation'):
+        gamma = (1 / (np.sqrt(1-c*c)))
+        gammar = round((1 / (np.sqrt(1-c*c))),3)
+        monthz = round((12-(12/gamma)),3)
+        st.success('The Time Dilation would be: {} times'.format(gammar))
+        st.text("Or you will experience: {} lesser month(s).".format(monthz))
 
 
 
